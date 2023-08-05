bb4bab5773a29e82a4968032d3f561e9cef91c65
Rahil Kothari <kotharirahil9@gmail.com>
2023-08-05 10:01:43 +0530

    Add register route

--- WhatsForDinner/src/App.jsx
+++ WhatsForDinner/src/App.jsx
@@ -2,17 +2,21 @@ import './App.css'
 import Login from './components/Login'
 import Signup from './components/Signup'
 import { AuthProvider } from './contexts/AuthContext'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
     
   return (
     <>
-      
-      <AuthProvider>
-          <Signup/>
-      </AuthProvider>
-        
-        <Login/>
+      <div>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route path='/register' element={<Signup/>} />
+            </Routes>
+          </AuthProvider>
+        </Router>
+      </div>
+      <h1>Hi</h1>
     </>
   )
 }
